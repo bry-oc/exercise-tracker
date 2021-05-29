892c37e540a49fb4d7b43e6e75e3d68be17179be
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-05-29 15:17:22 -0400

    Added userSchema and exerciseSchema. Started /api/users post request

--- server/index.js
+++ server/index.js
@@ -15,9 +15,25 @@ db.once("open", function() {
     console.log("Connection Successful!");
 })
 
+const exerciseSchema = new mongoose.Schema({
+    description: {type: String},
+    duration: {type: String},
+    date: {type: Date}
+});
+
+const userSchema = new mongoose.Schema({
+    username: {type: String},
+    exercises: [exerciseSchema]
+});
+
 app.get('/', (req, res) => {
     res.json({message: "Hello World!"});
 });
+
+app.post('/api/users', (req, res) => {
+    const user = req.body.username;
+
+})
 
 app.listen(PORT, () => {
     console.log(`App is listening on port ${PORT}`);
