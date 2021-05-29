5be5a0bb6c5882685619e6b9182b343139b798d6
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-05-29 15:39:43 -0400

    Added post request to /api/users

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
+const cors = require("cors");
 const dotenv = require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 
 const app =  express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cors({optionsSuccessStatus: 200}));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 mongoose.connect(process.env.MONGO_URI_TEST, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -26,14 +33,37 @@ const userSchema = new mongoose.Schema({
     exercises: [exerciseSchema]
 });
 
+const Exercise = mongoose.model('Exercise', exerciseSchema);
+const User = mongoose.model('User', userSchema);
+
 app.get('/', (req, res) => {
     res.json({message: "Hello World!"});
 });
 
+//create a new user
 app.post('/api/users', (req, res) => {
-    const user = req.body.username;
-
-})
+    const username = req.body.username;
+    User.findOne({username: username}, function(err, user){
+        if(err){
+            return console.error(err);
+        } else if (user) {
+            //if the user already exists, return it
+            console.log("user exists");
+            return res.json({username: username, _id: user._id});
+        } else {
+            //the username is not taken > create a new user
+            console.log("creating new user");
+            const newUser = new User({username: username});
+            newUser.save(function(err, user){
+                if(err){
+                    return console.error(err);
+                } else {
+                    return res.json({username: user.username, _id: user._id});
+                }
+            })
+        }
+    })
+});
 
 app.listen(PORT, () => {
     console.log(`App is listening on port ${PORT}`);
