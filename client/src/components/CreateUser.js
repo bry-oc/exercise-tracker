eb8bb52eaf4f6e603df72a39a516c69d675b0892
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-06-02 18:20:43 -0400

    Added create user functionality

--- client/src/components/CreateUser.js
+++ client/src/components/CreateUser.js
@@ -1,8 +1,43 @@
+import React from 'react';
+
 function CreateUser(){
+    const [userId, setUserId] = React.useState(null);
+
+    let createUser = (e) => {
+        e.preventDefault();
+        const username = e.target.username.value;
+        fetch('/api/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: username,
+            })
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                setUserId(data._id);
+            })
+    }
+
     return(
         <div className="wrapper">
-            <h2>Create User!</h2>
+            <div className="title">
+                <h2>Create User!</h2>
+            </div>
+            <form id="createuser" onSubmit={createUser}>
+                <label id="username-label">username</label>
+                <br></br>
+                <input id="username" type="text"></input>
+                <br></br>
+                <button id="user-submit" type="submit">Create User</button>
+            </form>
+            <div>
+                <p>{userId ? "user id: " +userId : ""}</p>
+            </div>
         </div>
+
     );
 }
 
--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -5,7 +5,7 @@ function Welcome(){
                 <h2>Welcome!</h2>
             </div>
             <div className="content">
-                <p>This website is an exercise tracker.  You can create an account with a unique username that will be assigned an id.  With the id given, you can add exercises to your account and you can use it to view your exercise log.  Your exercise log can be limited to within a date range and to only show a given number of exercises.  Please store your id somewhere safe.</p>
+                <p>This website is an exercise tracker.  You can create an account with a unique username that will be assigned an id.  With the id given, you can add exercises to your account and you can view your exercise log.  Your exercise log can be limited to within a date range and to only show a given number of exercises.</p>
             </div>
         </div>
     );
